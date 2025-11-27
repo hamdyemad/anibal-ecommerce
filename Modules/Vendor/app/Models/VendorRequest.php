b1722b431cb84b8453a2c195d46db60da4013915
hamdy emad <razereng0@gmail.com>
2025-11-27 17:57:33 +0200

    done

--- Modules/Vendor/app/Models/VendorRequest.php
+++ Modules/Vendor/app/Models/VendorRequest.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Vendor\app\Models;
 
+use App\Models\Traits\HumanDates;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,7 +10,7 @@ use Modules\CategoryManagment\app\Models\Activity;
 
 class VendorRequest extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, HumanDates;
 
     protected $table = 'vendor_requests';
 
