ebc62e3731fdf203b314f09d9c88b32be55c87b0
hamdy emad <razereng0@gmail.com>
2025-12-24 11:05:16 +0200

    done

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -7,31 +7,17 @@ use Illuminate\Auth\AuthenticationException;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
             return route('login');
         }
     }
-
-    /**
-     * Handle unauthenticated requests for API routes
-     */
     protected function unauthenticated($request, array $guards)
     {
         if ($request->expectsJson()) {
-            throw new AuthenticationException(
-                config('responses.unauthorized')[app()->getLocale()],
-                $guards
-            );
+            throw new AuthenticationException(config('responses.unauthorized')[app()->getLocale()], $guards);
         }
-
         parent::unauthenticated($request, $guards);
     }
 }
