db8517d52fe58b347ec0012a108a0c9eaa26bc2f
hamdy emad <razereng0@gmail.com>
2026-01-05 18:01:50 +0200

    done

--- Modules/SystemSetting/app/Http/Resources/Api/AdsResource.php
+++ Modules/SystemSetting/app/Http/Resources/Api/AdsResource.php
@@ -14,6 +14,28 @@ class AdsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        // Determine which width/height to return based on requested type
+        $requestedType = $request->input('type');
+        
+        // If type is specified, return dimensions for that type
+        // Otherwise return based on the ad's type array (prefer website if both)
+        if ($requestedType === 'mobile') {
+            $width = $this->mobile_width;
+            $height = $this->mobile_height;
+        } elseif ($requestedType === 'website') {
+            $width = $this->website_width;
+            $height = $this->website_height;
+        } else {
+            // Default: if ad supports website, use website dimensions, otherwise mobile
+            if (is_array($this->type) && in_array('website', $this->type)) {
+                $width = $this->website_width;
+                $height = $this->website_height;
+            } else {
+                $width = $this->mobile_width;
+                $height = $this->mobile_height;
+            }
+        }
+
         return [
             'id' => $this->id,
             'title' => $this->title,
@@ -22,10 +44,8 @@ class AdsResource extends JsonResource
             'link' => $this->link,
             'type' => $this->type,
             'position' => $this->position,
-            'mobile_width' => $this->mobile_width,
-            'mobile_height' => $this->mobile_height,
-            'website_width' => $this->website_width,
-            'website_height' => $this->website_height,
+            'width' => $width,
+            'height' => $height,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
